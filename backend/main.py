8938c6f68e6f2c6d3dd0a838303458bf2deef66f
Michael Walton <waltonmichael.e@gmail.com>
2026-04-12 15:13:48 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from services.regex_utils import insert_concat, to_postfix
+from services.nfa import postfix_to_nfa, nfa_to_dict
 
 app = FastAPI()
 
@@ -28,9 +29,12 @@ def convert(req: RegexRequest):
 
     with_concat = insert_concat(raw_regex)
     postfix = to_postfix(with_concat)
+    nfa = postfix_to_nfa(postfix)
+    nfa_data = nfa_to_dict(nfa)
 
     return {
         "input": raw_regex,
         "with_concat": with_concat,
-        "postfix": postfix
+        "postfix": postfix,
+        "nfa": nfa_data
     }
